c31d7339181091bc917e3ad42196c00b2344cdc9
srikanthanagaddala <133683841+srikanthanagaddala@users.noreply.github.com>
2024-01-09 20:33:26 +0530

    Update backend.tf

--- backend.tf
+++ backend.tf
@@ -1,8 +1,8 @@
 terraform {
   backend "s3" {
-    bucket         = "mrcloudbook-cicd-bucket"
+    bucket         = "srikanth51"
     key            = "my-terraform-environment/main"
     region         = "ap-south-1"
-    dynamodb_table = "mrcloudbook-dynamo-db-table"
+    dynamodb_table = "srikanth-dynamo-db-table"
   }
 }
